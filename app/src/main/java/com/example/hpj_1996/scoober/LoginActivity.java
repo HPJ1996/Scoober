4de954989cda403c272239ce207e98019217d1b1
Pang Jen <Hung>
2016-04-18 14:33:17 +0800

    T1

--- app/src/main/java/com/example/hpj_1996/scoober/LoginActivity.java
+++ app/src/main/java/com/example/hpj_1996/scoober/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.hpj_1996.scoober;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,24 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setTab();
+    }
+
+    private void setTab()
+    {
+        TabHost tabHost = (TabHost)findViewById(R.id.tabHost);
+        tabHost.setup();
+
+        TabHost.TabSpec tabSpec = tabHost.newTabSpec("login");
+        tabSpec.setContent(R.id.login);
+        tabSpec.setIndicator("login");
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("register");
+        tabSpec.setIndicator("register");
+        tabSpec.setContent(R.id.register);
+        tabHost.addTab(tabSpec);
+
+        tabHost.setCurrentTab(0);
     }
 }

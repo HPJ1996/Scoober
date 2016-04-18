b0c79bde4d7203fa75524e359540779681e6905f
Pang Jen <Hung>
2016-04-18 15:02:42 +0800

    T1

--- app/src/main/java/com/example/hpj_1996/scoober/LoginActivity.java
+++ app/src/main/java/com/example/hpj_1996/scoober/LoginActivity.java
@@ -4,18 +4,30 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TabHost;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Button login_button;
+    private Button login_button;
+    private AutoCompleteTextView account;
+    private EditText password;
+
+    private final int ACCOUNT = 0;
+    private final int PASSWORD = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         setTab();
+
+        account = (AutoCompleteTextView)findViewById(R.id.prompt_account);
+        password = (EditText)findViewById(R.id.password);
 
         login_button = (Button)findViewById(R.id.action_sign_in);
         login_button.setOnClickListener(loginButtonListener);
@@ -25,8 +37,14 @@ public class LoginActivity extends AppCompatActivity {
 
         public void onClick(View v) {
             Intent intent = new Intent();
-            intent.setClass(LoginActivity.this,MapsActivity.class);
+            intent.setClass(LoginActivity.this, MapsActivity.class);
+//            intent.putExtra(ACCOUNT, account.getText().toString());
+//            intent.putExtra(PASSWORD, password.getText().toString());
+
             //startActivityForResult(intent, REQUEST_INPUT_NAME);
+
+            startActivity(intent);
+
         }
     };
 

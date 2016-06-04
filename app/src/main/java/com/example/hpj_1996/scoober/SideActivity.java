bd5b01253d90f03627d375a51e56502e441c9b79
Pang Jen <Hung>
2016-06-04 15:25:46 +0800

    MAP

--- app/src/main/java/com/example/hpj_1996/scoober/SideActivity.java
+++ app/src/main/java/com/example/hpj_1996/scoober/SideActivity.java
@@ -27,6 +27,17 @@ public class SideActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(SideActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);

a87519e09a6e6b8afc6d0118ea01b5ab44e3c806
Jack <z2589906@gmail.com>
2016-06-05 13:38:14 +0800

    account check

--- app/src/main/java/com/example/hpj_1996/scoober/LoginActivity.java
+++ app/src/main/java/com/example/hpj_1996/scoober/LoginActivity.java
@@ -28,7 +28,7 @@ public class LoginActivity extends AppCompatActivity {
     private Button loginButton;
     private AutoCompleteTextView account;
     private EditText password;
-    private TextView errorMessage;
+    private TextView loginErrorMessage;
 
     //Register-component
     private EditText registerName;
@@ -36,14 +36,12 @@ public class LoginActivity extends AppCompatActivity {
     private EditText registerPassword;
     private EditText registerTwicePassword;
     private Button registerButton;
+    private TextView registerErrorMessage;
 
-    //Data base
-    public final static String ACCOUNT_TABLE = "ACCOUNT_TABLE";
-    public final static String CREATETABLE = "create table " +
-            ACCOUNT_TABLE + "(name, account, password);";
+    //Account Data Base
+    private AccountDataBase accountDataBase;
 
-    private SQLiteDatabase db;
-    private DBOpenHelper openhelper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,7 +54,7 @@ public class LoginActivity extends AppCompatActivity {
         password = (EditText)findViewById(R.id.password);
         loginButton = (Button)findViewById(R.id.action_sign_in);
         loginButton.setOnClickListener(loginListener);
-        errorMessage = (TextView)findViewById(R.id.error_message);
+        loginErrorMessage = (TextView)findViewById(R.id.login_error_message);
 
         //Register-component
         registerAccount = (AutoCompleteTextView)findViewById(R.id.register_account);
@@ -65,60 +63,44 @@ public class LoginActivity extends AppCompatActivity {
         registerTwicePassword = (EditText)findViewById(R.id.register_twice_password);
         registerButton = (Button)findViewById(R.id.action_register);
         registerButton.setOnClickListener(registerListener);
+        registerErrorMessage = (TextView)findViewById(R.id.register_error_message);
 
-        //Account data base
-        openhelper = new DBOpenHelper(this);
-        db = openhelper.getWritableDatabase();
+        accountDataBase = new AccountDataBase(this);
+
     }
 
     private View.OnClickListener loginListener = new View.OnClickListener() {
 
         public void onClick(View v) {
-            if(!checkAccount(account.getText().toString(), password.getText().toString())) {
-                return;
-            }
+        if(accountDataBase.verifyAccount(account.getText().toString(), password.getText().toString())) {
+
             Intent intent = new Intent();
             intent.setClass(LoginActivity.this, SideActivity.class);
             intent.putExtra("account", account.getText().toString());
-
             startActivity(intent);
+        }else{
+            loginErrorMessage.setText("Account or password error!");
+        }
         }
     };
 
     private View.OnClickListener registerListener = new View.OnClickListener() {
 
         public void onClick(View v) {
-            ContentValues cv = new ContentValues();
-            cv.put("name", registerName.getText().toString());
-            cv.put("account", registerAccount.getText().toString());
-            cv.put("password", registerPassword.getText().toString());
+        String name = registerName.getText().toString();
+        String account = registerAccount.getText().toString();
+        String password = registerPassword.getText().toString();
 
-            Log.d("register", "name: " + cv.get("name"));
-            Log.d("register", "account: " + cv.get("account"));
-            Log.d("register", "password: " + cv.get("password"));
-
-            db.insert(ACCOUNT_TABLE, null, cv);
-
+        try{
+            accountDataBase.insertAccount(name, account, password);
             tabHost.setCurrentTab(0);
             Toast.makeText(LoginActivity.this,"Complete register!", Toast.LENGTH_SHORT).show();
+
+        }catch(RegisterFailedException e){
+            registerErrorMessage.setText(e.getMessage());
+        }
         }
     };
-
-    private boolean checkAccount(String account, String password){
-        Cursor cursor = db.rawQuery("select * from " + ACCOUNT_TABLE, null);
-
-        cursor.moveToFirst();
-        for (int i = 0; i < cursor.getCount(); i++) {
-            if(cursor.getString(1).equals(account)){
-                if(cursor.getString(2).equals(password)){
-                    return true;
-                }else break;
-            }
-            cursor.moveToNext();
-        }
-        errorMessage.setText("Account or password error!");
-        return false;
-    }
 
 
     private void setTab(){
@@ -137,23 +119,141 @@ public class LoginActivity extends AppCompatActivity {
 
         tabHost.setCurrentTab(0);
     }
+}
 
+class AccountDataBase{
+    public final static String ACCOUNT_TABLE = "ACCOUNT_TABLE";
+    public final static String CREATE_TABLE = "create table " +
+            ACCOUNT_TABLE + "(name, account, password);";
+
+    private SQLiteDatabase database;
+
+    public AccountDataBase(Context context){
+        DBOpenHelper openhelper = new DBOpenHelper(context);
+        database = openhelper.getWritableDatabase();
+    }
+
+    public void checkRegisterValid(String name, String account, String password)
+            throws RegisterFailedException {
+        String errorMessage = "";
+        if (name.equals("")) {
+            errorMessage += "Name cannot be empty string!\n";
+        }
+        if (account.equals("")) {
+            errorMessage += "Account cannot be empty string!\n";
+        }
+        if (password.equals("")) {
+            errorMessage += "Password cannot be empty string!\n";
+        }
+        if (existAccount(account)) {
+            errorMessage += "Exist this account\n";
+        }
+
+        if (!errorMessage.equals(""))
+            throw new RegisterFailedException(errorMessage);
+    }
+
+    public void insertAccount(String name, String account, String password)
+            throws RegisterFailedException{
+        checkRegisterValid(name, account, password);
+        ContentValues cv = new ContentValues();
+        cv.put("name", name);
+        cv.put("account", account);
+        cv.put("password", password);
+
+        database.insert(ACCOUNT_TABLE, null, cv);
+    }
+
+    public Cursor findAccount(String account) throws NotFoundAccountException{
+        Cursor cursor = database.rawQuery("select * from " + ACCOUNT_TABLE, null);
+
+        cursor.moveToFirst();
+        for (int i = 0; i < cursor.getCount(); i++) {
+            if(cursor.getString(1).equals(account)){
+                return cursor;
+            }
+            cursor.moveToNext();
+        }
+        throw new NotFoundAccountException("Not found account: " + account);
+    }
+
+    public boolean existAccount(String account){
+        try{
+            findAccount(account);
+            return true;
+        }catch(NotFoundAccountException e){
+            return false;
+        }
+    }
+
+
+    public boolean verifyAccount(String account, String password){
+        Cursor cursor = database.rawQuery("select * from " + ACCOUNT_TABLE, null);
+
+        cursor.moveToFirst();
+        for (int i = 0; i < cursor.getCount(); i++) {
+            if(cursor.getString(1).equals(account)){
+                if(cursor.getString(2).equals(password)){
+                    cursor.close();
+                    return true;
+                }else break;
+            }
+            cursor.moveToNext();
+        }
+        cursor.close();
+        return false;
+    }
+
+    public void updateAccount(String name, String account)throws UpdateFailedException{
+        if(existAccount(account))throw new UpdateFailedException("exist same account!");
+
+        try{
+            checkRegisterValid(name, account, "xxx");
+        }catch(RegisterFailedException e){
+            throw new UpdateFailedException(e.getMessage());
+        }
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name", name);
+        contentValues.put("account", account);
+        database.update(ACCOUNT_TABLE, contentValues, "account='" + account + "'", null);
+    }
 
 }
 
 class DBOpenHelper extends SQLiteOpenHelper {
+
     public DBOpenHelper(Context context) {
         super(context, "accounts.db", null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(LoginActivity.CREATETABLE);
+        db.execSQL(AccountDataBase.CREATE_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldV, int newV) {
     }
 }
+
+class RegisterFailedException extends Exception{
+    public RegisterFailedException(String message){
+        super(message);
+    }
+}
+
+class UpdateFailedException extends Exception{
+    public UpdateFailedException(String message){
+        super(message);
+    }
+}
+
+class NotFoundAccountException extends Exception{
+    public NotFoundAccountException(String message){
+        super(message);
+    }
+}
+
 
 
--- app/src/main/java/com/example/hpj_1996/scoober/PersonalInfoActivity.java
+++ app/src/main/java/com/example/hpj_1996/scoober/PersonalInfoActivity.java
@@ -1,7 +1,5 @@
 package com.example.hpj_1996.scoober;
 
-import android.content.ContentValues;
-import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
@@ -14,17 +12,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class PersonalInfoActivity extends AppCompatActivity {
-    private final static String NAME = "NAME";
-    private final static String ACCOUNT = "ACCOUNT";
-    private final static String PERSONAL_INFO = "PERSONAL_INFO";
 
     private EditText editName;
     private EditText editAccount;
     private TextView fixPassword;
     private SQLiteDatabase db;
-    private Button applyPersonalInfo;
-    String account;
-    Cursor accountCursor;
+    private Button modifyPersonalInfo;
+    private AccountDataBase accountDataBase;
+    private String account;
+    private Cursor accountCursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,54 +30,36 @@ public class PersonalInfoActivity extends AppCompatActivity {
         editName = (EditText)findViewById(R.id.edit_name);
         editAccount = (EditText)findViewById(R.id.edit_account);
         fixPassword = (TextView)findViewById(R.id.fix_password);
-        applyPersonalInfo = (Button)findViewById(R.id.apply_personal_info);
+        modifyPersonalInfo = (Button)findViewById(R.id.modify_personal_info);
+        accountDataBase = new AccountDataBase(this);
 
         try{
             account = getIntent().getStringExtra("account");
-            accountCursor = findAccount(account);
-            applyPersonalInfo.setOnClickListener(new ApplyListener());
-
-            Log.d("info", "name: " + accountCursor.getString(0));
-            Log.d("info", "account: " + accountCursor.getString(1));
-            Log.d("info", "password: " + accountCursor.getString(2));
+            accountCursor = accountDataBase.findAccount(account);
+            modifyPersonalInfo.setOnClickListener(new ModifyListener());
 
             //set personal info
             editName.setText(accountCursor.getString(0));
             editAccount.setText(accountCursor.getString(1));
             fixPassword.setText(accountCursor.getString(2));
         }catch (NotFoundAccountException e){
-            Log.d("info", e.getMessage());
+            Log.d("error", e.getMessage());
         }
     }
 
-    private Cursor findAccount(String account) throws NotFoundAccountException{
-        db = new DBOpenHelper(this).getWritableDatabase();
-        Cursor cursor = db.rawQuery("select * from " + LoginActivity.ACCOUNT_TABLE, null);
-
-        cursor.moveToFirst();
-        for (int i = 0; i < cursor.getCount(); i++) {
-            if(cursor.getString(1).equals(account)){
-                return cursor;
-            }
-            cursor.moveToNext();
-        }
-        throw new NotFoundAccountException("Not found account: " + account);
-    }
-
-    class ApplyListener implements View.OnClickListener{
+    class ModifyListener implements View.OnClickListener{
 
         public void onClick(View v){
-            ContentValues contentValues = new ContentValues();
-            contentValues.put("name", editName.getText().toString());
-            contentValues.put("account", editAccount.getText().toString());
-            db.update(LoginActivity.ACCOUNT_TABLE, contentValues, "account='" + account + "'", null);
-            Log.d("info","change name: " + accountCursor.getString(0));
+            try{
+                accountDataBase.updateAccount(editName.getText().toString(),
+                        editAccount.getText().toString());
+                finish();
+            }catch(UpdateFailedException e){
+                Toast.makeText(PersonalInfoActivity.this,
+                        e.getMessage(),
+                        Toast.LENGTH_SHORT).show();
+            }
         }
     };
 }
 
-class NotFoundAccountException extends Exception{
-    public NotFoundAccountException(String message){
-        super(message);
-    }
-}

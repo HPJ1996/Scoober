b0fbcb7929b3bcebe5f4908b39c5d626720318c5
sheu <leo850611@gmail.com>
2016-06-18 19:03:37 +0800

    add recordList 50%

--- app/src/main/java/com/example/hpj_1996/scoober/RecordActivity.java
+++ app/src/main/java/com/example/hpj_1996/scoober/RecordActivity.java
@@ -2,11 +2,32 @@ package com.example.hpj_1996.scoober;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class RecordActivity extends AppCompatActivity {
+    private ListView listView;
+    private String[] list = {"120.648274 ,24.179955"};
+    private ArrayAdapter<String> listAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+
+        listView = (ListView)findViewById(R.id.list_view);
+        listAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,list);
+        listView.setAdapter(listAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "位置: " + list[position], Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }

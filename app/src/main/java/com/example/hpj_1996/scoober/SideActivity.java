49efa29b61f76cdb45a4b1e9f95b2e518de7e08a
Pang Jen <Hung>
2016-06-20 18:20:26 +0800

    maybe final

--- app/src/main/java/com/example/hpj_1996/scoober/SideActivity.java
+++ app/src/main/java/com/example/hpj_1996/scoober/SideActivity.java
@@ -81,12 +81,25 @@ public class SideActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
 
-        locationMgr = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (locationMgr.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
-            provider = LocationManager.NETWORK_PROVIDER;
-        } else if (locationMgr.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            provider = LocationManager.GPS_PROVIDER;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 123);
+            return;
         }
+
+        locationMgr = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (locationMgr.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            provider = LocationManager.GPS_PROVIDER;
+        } else if (locationMgr.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            provider = LocationManager.NETWORK_PROVIDER;
+        }
+
         //Location Listener
         long minTime = 5000;//ms
         float minDist = 5.0f;//meter
